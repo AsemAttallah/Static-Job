e3a874a6a824a181ce0759649c58159e1f668ac3
asem <asem@asem-attallah-pc.scoiumtech.local>
2023-10-01 15:30:42 +0300

    final

--- main.js
+++ main.js
@@ -1,40 +1,79 @@
+import data from "./data.json" assert { type: "json" };
+const resultContainer = document.querySelector(".resultContainer"); //posts-div
 const searchSelected = document.querySelector(".search-selected"); //labels-section
 const searchValue = document.querySelector(".searchBar-inputField"); // text-input
-
 const noResult = document.querySelector(".noResult");
-const numResult = [];
 
-const uniquefined = new Set();
+// let uniquefined = new Set();
+let uniqueFindsArray = [];
+let renderedData = [];
 
-console.log(searchValue.value);
-const resultContainer = document.querySelector(".resultContainer"); //posts-div
-async function render() {
-  const getData = async () => {
-    const res = await fetch("data.json");
-    const data = await res.json();
-    return data;
-  };
-  const data = await getData();
-  console.log(data);
+function fillterData(data) {
+  data.map((el) => {
+    const filltered = [
+      el.level.toLowerCase(),
+      el.role.toLowerCase(),
+      ...el.languages.map((lang) => lang.toLowerCase()),
+      ...el.tools.map((tool) => tool.toLowerCase()),
+    ];
+    uniqueFindsArray = searchBarChanges(filltered).map((el) =>
+      el.toLowerCase()
+    );
+    const fillteredResult = uniqueFindsArray.every((element) =>
+      filltered.includes(element)
+    );
+    if (fillteredResult) {
+      renderedData.push(el);
+    }
+  });
+  renderAll(renderedData);
+}
+fillterData(data);
 
-//   fillteredDAta=data.map((el)=>{
+function searchBarChanges() {
+  searchValue.addEventListener("keydown", (event) => {
+    if (event.keyCode == "13") {
+      const addfilter = document.createElement("h4");
+      addfilter.classList.add("addfilter");
+      if (!uniqueFindsArray.includes(searchValue.value)) {
+        addfilter.innerText = searchValue.value;
+        searchSelected.appendChild(addfilter);
+        uniqueFindsArray.push(searchValue.value);
+        fillterData(data);
+      }
+      addfilter.addEventListener("click", () => {
+        const textToRemove = addfilter.innerText;
+        const indexToRemove = uniqueFindsArray.indexOf(textToRemove);
+        uniqueFindsArray.splice(indexToRemove, 1);
+        addfilter.remove();
+        fillterData(data);
+      });
+    }
+  });
+  return uniqueFindsArray;
+}
 
-//   })
+function renderAll(data) {
+  resultContainer.innerHTML = "";
+  renderedData = [];
+  if (data.length == 0) {
+    noResult.style.display = "block";
+  } else {
+    noResult.style.display = "none";
+  }
 
   data.map((el) => {
-    // all posts with HTML and correct Data (waiting to add to DOM)
-    const allvalues = [];
     // create
-    const containerLandR = document.createElement("div"); // include left and right side
+    const containerLandR = document.createElement("div");
     containerLandR.classList.add("containerLandR-class");
-    const RightContainer = document.createElement("div"); // just loop for a some feild to fill it
+    const RightContainer = document.createElement("div");
     RightContainer.classList.add("RightContainer-class");
-    const leftContainer = document.createElement("div"); // include all left side in the card
+    const leftContainer = document.createElement("div");
     leftContainer.classList.add("leftContainer-class");
 
     const image = document.createElement("img");
     image.classList.add("image-class");
-    const allInfo = document.createElement("div"); // all without image
+    const allInfo = document.createElement("div");
     allInfo.classList.add("allInfo");
     const firstLine = document.createElement("div");
     firstLine.classList.add("firstLine-class");
@@ -62,8 +101,6 @@ async function render() {
     role.classList.add("right-elements");
     const level = document.createElement("h4");
     level.classList.add("right-elements");
-    // const languages=document.createElement("p");
-    // const tools=document.createElement("p");
 
     /// add data
     //left
@@ -84,13 +121,9 @@ async function render() {
 
     //right
     role.innerText = el.role;
-    allvalues.push(el.role);
     level.innerText = el.level;
-    allvalues.push(el.level);
 
-    finder(searchValue, allvalues, containerLandR); // display: none??
     // append
-
     //left
     leftContainer.appendChild(image);
     firstLine.appendChild(company);
@@ -117,71 +150,17 @@ async function render() {
       const languages = document.createElement("h4");
       languages.classList.add("right-elements");
       languages.innerText = el.languages[i];
-      allvalues.push(el.languages[i]);
       RightContainer.appendChild(languages);
     }
 
     for (let i = 0; i < el.tools.length; i++) {
       const tools = document.createElement("h4");
       tools.innerText = el.tools[i];
-      allvalues.push(el.tools[i]);
       tools.classList.add("right-elements");
       RightContainer.appendChild(tools);
     }
 
     containerLandR.appendChild(RightContainer);
     resultContainer.appendChild(containerLandR);
-  });
-}
-render();
-
-
-function finder(searchValue, allvalues, containerLandR) {
-
-  searchValue.addEventListener("change", () => {
-    // textbox listener
-    //   console.log(allSearchValues);
-    if (allvalues.includes(searchValue.value)) {
-      // To add the new label to label section and store it in the SET.
-      const addfilter = document.createElement("h4");
-      addfilter.classList.add("addfilter");
-      //   allSearchValues.push(searchValue.value)
-      if (!uniquefined.has(searchValue.value)) {
-        addfilter.innerText = searchValue.value;
-        searchSelected.appendChild(addfilter);
-        uniquefined.add(searchValue.value);
-      }
-
-      addfilter.addEventListener("click", () => {
-        addfilter.remove();
-        uniquefined.delete(searchValue.value);
-        console.log("cheeeck", uniquefined);
-        finder(searchValue, allvalues, containerLandR);
-        // render();
-        // checkAll(uniquefined);
-      });
-
-      function checkAll(uniquefined) {
-        console.log(uniquefined);
-        const uniqueNumbersArray = [...uniquefined];
-        // numResult.push(uniqueNumbersArray);
-        console.log("arr", uniqueNumbersArray);
-        const allIncluded = uniqueNumbersArray.every((element) =>
-          allvalues.includes(element)
-        );
-        console.log("aaaaaaaaaaaa", allIncluded); // const allIncluded = true;
-        if (allIncluded) {
-          console.log(true); // All elements in 'a' are included in 'b'
-          containerLandR.style.display = "flex";
-        } else {
-          console.log("are we here??");
-          containerLandR.style.display = "none";
-        }
-      }
-      checkAll(uniquefined);
-    } else {
-      // searchValue.value=""
-      containerLandR.style.display = "none";
-    }
   });
 }
